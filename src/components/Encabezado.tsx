1a1fa7cbce1d8df3db1c7e3a9de6afd56c96a212
JonathanArevaloS <jarevalosilva10@gmail.com>
2024-02-08 09:39:22 -0500

    Jonathan - Modificacion Encabezado
    Encabezado

--- src/components/Encabezado.tsx
+++ src/components/Encabezado.tsx
@@ -1,19 +1,7 @@
 // import React, { ReactNode } from 'react'
 // import '../styles/style.scss'
 
-// interface Props{
-//     children: ReactNode
-//     title: string
-   
-// }
-// const Encabezado = ({title,  children}:Props) => {
-//   return (
-//     <div className="container text-white">
-//         <h1>{title}</h1>
-//       {children}
-//     </div>
-//   )
-// }
+
 
 // export default Encabezado
 
